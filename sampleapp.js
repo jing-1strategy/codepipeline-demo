7b338e220c6dd5a6aa8e01bb043e7a7721474773
Jing Liang <jing@1strategy.com>
2019-06-07 16:01:19 -0700

    Initial Commit

---
+++ sampleapp.js
@@ -0,0 +1,55 @@
+const http = require('http');
+const { parse } = require('querystring');
+
+const hostname = '0.0.0.0';
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+   if (req.method === 'POST') {
+       collectRequestData(req, result => {
+           console.log(result);
+           res.end(`
+               http method: ${req.method}
+               Name: ${result.fname}
+               Age: ${result.age}
+           `);
+       });
+   }
+   else {
+       res.statusCode = 200;
+       res.setHeader('Content-Type', 'text/html');
+       res.end(`
+           <!doctype html>
+           <html>
+           <body>
+               <h1>http method: ${req.method} </h1>
+               <form action="/" method="post">
+                   <input type="text" name="fname" /><br />
+                   <input type="number" name="age" /><br />
+                   <button>Save</button>
+               </form>
+           </body>
+           </html>
+       `);
+   }
+});
+
+function collectRequestData(request, callback) {
+   const FORM_URLENCODED = 'application/x-www-form-urlencoded';
+   if(request.headers['content-type'] === FORM_URLENCODED) {
+       let body = '';
+       request.on('data', chunk => {
+           body += chunk.toString();
+       });
+       request.on('end', () => {
+           callback(parse(body));
+       });
+   }
+   else {
+       callback(null);
+   }
+}
+
+server.listen(port, hostname, () => {
+   console.log(`Server running at http://${hostname}:${port}/`);
+});
