9fd680c378d612b82dae7a861099284b2314d963
Jing Liang <jing@1strategy.com>
2019-06-13 15:02:07 -0700

    update config

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -11,10 +11,6 @@ cat <<EOF >> /etc/nginx/conf.d/ssl.conf
 location / {
                   proxy_pass http://0.0.0.0:8080;
                   proxy_http_version 1.1;
-                  proxy_set_header Upgrade $http_upgrade;
-                  proxy_set_header Connection 'upgrade';
-                  proxy_set_header Host $host;
-                  proxy_cache_bypass $http_upgrade;
               }
           }
 EOF
