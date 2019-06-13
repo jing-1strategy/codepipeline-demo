87a1f86f1c6531d9cfd68818a526cd2c26a33bb3
Jing Liang <jing@1strategy.com>
2019-06-13 10:58:37 -0700

    more updates

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -4,5 +4,18 @@ set -e
 cd ~/node
 npm install
 
-yes | cp -rf /webapps/configurations/nginx.conf /etc/nginx/nginx.conf
-nginx -s reload
+sed '$d' /etc/nginx/conf.d/ssl.conf
+
+cat <<EOF >> /etc/nginx/conf.d/ssl.conf
+location / {
+                  proxy_pass http://0.0.0.0:8080;
+                  proxy_http_version 1.1;
+                  proxy_set_header Upgrade $http_upgrade;
+                  proxy_set_header Connection 'upgrade';
+                  proxy_set_header Host $host;
+                  proxy_cache_bypass $http_upgrade;
+              }
+          }
+EOF
+
+nginx -t && nginx -s reload
