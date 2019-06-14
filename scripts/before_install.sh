c495b9d7f61a138ff50e8a6679dbedf0f57cea62
Jing Liang <jing@1strategy.com>
2019-06-13 20:53:25 -0700

    test

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -8,8 +8,3 @@ yum -y install nodejs
 # install pm2 module globaly
 npm install -g pm2
 pm2 update
-pm2 startup systemd -u ec2-user --hp /home/ec2-user
-# # update default nginx.conf
-# unalias cp
-# yes | cp -rf /webapps/configurations/nginx.conf /etc/nginx/nginx.conf
-# nginx -s reload
