aa34bb7e2922e7f554e0d54a2b64b80aed0559d5
Jing Liang <jing@1strategy.com>
2019-06-13 17:58:04 -0700

    add version

--- sampleapp.js
+++ sampleapp.js
@@ -5,24 +5,25 @@ const hostname = '0.0.0.0';
 const port = 8080;
 
 const server = http.createServer((req, res) => {
-   if (req.method === 'POST') {
-       collectRequestData(req, result => {
-           console.log(result);
-           res.end(`
+    if (req.method === 'POST') {
+        collectRequestData(req, result => {
+            console.log(result);
+            res.end(`
                http method: ${req.method}
                Name: ${result.fname}
                Age: ${result.age}
            `);
-       });
-   }
-   else {
-       res.statusCode = 200;
-       res.setHeader('Content-Type', 'text/html');
-       res.end(`
+        });
+    }
+    else {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(`
            <!doctype html>
            <html>
            <body>
                <h1>http method: ${req.method} </h1>
+               <h2>version 1.0</h2>
                <form action="/" method="post">
                    <input type="text" name="fname" /><br />
                    <input type="number" name="age" /><br />
@@ -31,25 +32,25 @@ const server = http.createServer((req, res) => {
            </body>
            </html>
        `);
-   }
+    }
 });
 
 function collectRequestData(request, callback) {
-   const FORM_URLENCODED = 'application/x-www-form-urlencoded';
-   if(request.headers['content-type'] === FORM_URLENCODED) {
-       let body = '';
-       request.on('data', chunk => {
-           body += chunk.toString();
-       });
-       request.on('end', () => {
-           callback(parse(body));
-       });
-   }
-   else {
-       callback(null);
-   }
+    const FORM_URLENCODED = 'application/x-www-form-urlencoded';
+    if (request.headers['content-type'] === FORM_URLENCODED) {
+        let body = '';
+        request.on('data', chunk => {
+            body += chunk.toString();
+        });
+        request.on('end', () => {
+            callback(parse(body));
+        });
+    }
+    else {
+        callback(null);
+    }
 }
 
 server.listen(port, hostname, () => {
-   console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
